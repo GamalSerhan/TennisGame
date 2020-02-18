1188937d35894f64f061099e9b3a79a43222c32d
GamalSerhan <53912162+GamalSerhan@users.noreply.github.com>
2020-02-18 10:46:59 -0400

    Update TennisGame2.java

--- TennisGame/src/TennisGame2.java
+++ TennisGame/src/TennisGame2.java
@@ -59,15 +59,7 @@ public class TennisGame2 implements TennisGame
 	private String normal3(String score) {
 		if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
-            score = P1res + "-" + P2res;
+			score = getLiteral(P1point) + "-" + getLiteral(P2point);
         }
 		return score;
 	}
